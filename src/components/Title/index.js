f7bba48505bde53fb8e402a66ac4413241a0777f
JeheeChoi <writetojehee@gmail.com>
2021-04-27 16:38:00 -0700

    Small update on about text

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,8 +5,7 @@ const About = () => {
     <div id="aboutme" className="jumbotron jumbotron-fluid m-0">
       <div className="container container-fluid p-5">
         <div className="row">
-
-          <h1 className="display-4 mb-5 text-center">No.1 Pizza in San Diego</h1>
+          <h3 className="display-6 mb-5 text-center">No.1 Pizza in San Diego</h3>
           <p className="lead text-center"></p>
         </div>
       </div>
