875d5cee5f2407ce586f72877fa8b7c976f8482f
JeheeChoi <writetojehee@gmail.com>
2021-06-22 10:16:21 -0700

    Update header
    added logo image and made the nav bar sticky

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,14 +25,20 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
+  <Navbar bg="dark" variant="dark" expand="md" sticky="top">
     <Navbar.Brand href="#">
+      <img
+        alt=""
+        src="https://i.imgur.com/7GCDsuS.jpg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{' '}
       Binx Pizza
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
         { alwaysOptions }
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
