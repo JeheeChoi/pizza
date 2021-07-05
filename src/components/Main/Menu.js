c123c1ab70aadfc6951c8c8f59c2e034b8967423
JeheeChoi <writetojehee@gmail.com>
2021-07-04 22:35:59 -0700

    Created menu component
    Menu component with full menu button

---
+++ src/components/Main/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import Image from 'react-bootstrap/Image'
+
+const Menu = () => {
+  return (
+    <div>
+      <div id="menu" className="jumbotron jumbotron-fluid bg-transparent m-0">
+        <div className="container-fluid">
+          <h1 className="display-4 pb-5">Menu</h1>
+          <a
+            className="btn btn-outline-dark btn-lg"
+            href="#menu"
+            role="button"
+            aria-label="Check out our menu"
+          >
+            Full Menu
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Menu
